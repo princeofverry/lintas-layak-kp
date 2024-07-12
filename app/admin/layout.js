ad3baef81f1c6f422b582e4559a5a5bca9ad8d67
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-13 06:27:51 +0700

    tambahin layout masing-masing page

---
+++ app/admin/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Admin Page",
+  description: "Admin section of the application",
+};
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -12,7 +12,7 @@ const AdminPage = () => {
     <div className="flex relative">
       <div className="bg-[#F3F3F3] mx-auto my-[110px] w-[1000px] h-[520px] rounded-lg flex justify-center">
         <div className="flex flex-row justify-center px-8">
-          <Image src={ilustrasi} alt="Ilustrasi Login" className="w-10/12 m-auto"/>
+          <Image src={ilustrasi} alt="Ilustrasi Login" className="w-10/12 m-auto" />
           <div>
             <h1 className="font-bold text-3xl text-[#3A4750] mt-20 mb-3">
               Selamat Datang Kembali !
@@ -22,11 +22,11 @@ const AdminPage = () => {
             </p>
             <div>
               <input placeholder="Username" type="text" id="username"
-              className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-5"/>
+                className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-5" />
               <input placeholder="Email" type="text" id="email"
-              className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-5"/>
+                className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-5" />
               <input placeholder="Password" type="password" id="password"
-              className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-7 "/>
+                className="outline outline-[#3A4750] shadow-md bg-transparent w-2/3 rounded-lg py-2 px-3 leading-3 mb-7 " />
               <div className="flex justify-center ">
                 <Button className="bg-[#2185D5] font-bold px-11 hover:bg-[#235985]">LOGIN</Button>
               </div>
@@ -35,7 +35,7 @@ const AdminPage = () => {
         </div>
       </div>
     </div>
-  </div>;
+  </div>
 };
 
 export default AdminPage;
