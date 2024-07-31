b8e8e94113f31e1a53d54058147fe32e0216501b
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-31 07:33:58 +0700

    try to fix

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -4,8 +4,11 @@ import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Admin Page",
+  title: "Lintas Layak - Admin",
   description: "Admin section of the application",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function Layout({ children }) {
